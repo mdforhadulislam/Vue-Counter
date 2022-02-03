26dbfd5981f28e44405a8239c244494cdbfe3c16
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-02-04 00:08:16 +0600

    counter file

---
+++ src/Components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Count {{ count }}</h1>
+  <button @click="count++">+</button>
+  <button @click="count--">-</button>
+</template>
+
+<script>
+export default {
+  name: "CounterApp",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+}
+</style>
---
+++ src/RootApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <Counter />
+  <Counter />
+</template>
+<script>
+import Counter from "./Components/Counter.vue";
+export default {
+  name: "RootApp",
+  components: {
+    Counter,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+}
+</style>
